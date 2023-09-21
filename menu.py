d48379f1ae6105ea23eed8b84d64183648263ec2
Anatole Sot <47571181+ano0002@users.noreply.github.com>
2023-09-21 18:20:34 +0200

    Finised menu

--- menu.py
+++ menu.py
@@ -5,7 +5,7 @@ class MainMenu(Entity):
         super().__init__(parent=camera.ui,add_to_scene_entities=add_to_scene_entities, **kwargs)
         self.play_button = Button(text="Play",text_origin=Vec2(-.3,0),model="quad",texture="button",scale=(.3,.1),position=(-.5,-0.15),color=color.white,highlight_scale=1.05,on_click=self.play,parent=self)
         self.settings_button = Button(text="Settings",text_origin=Vec2(-.3,0),model="quad",texture="button",scale=(.3,.1),position=(-.5,-.3),color=color.white,highlight_scale=1.05,on_click=self.open_settings,parent=self)
-        
+        self.background = Entity(model="quad",texture="menu",scale=(1*camera.aspect_ratio,1),parent=self,z=1)
         self.play_func = play
         self.settings_func = open_settings
     

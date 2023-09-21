3925eca268cb3667987891951b4ddc0e531f6e90
Anatole Sot <47571181+ano0002@users.noreply.github.com>
2023-09-21 18:09:14 +0200

    Added a bug

--- menu.py
+++ menu.py
@@ -1,12 +1,12 @@
 from ursina import *
 
-
+Text.default_font = "ethnocentric rg.otf"
 class MainMenu(Entity):
     def __init__(self, add_to_scene_entities=True, **kwargs):
-        super().__init__(add_to_scene_entities, **kwargs)
+        super().__init__(parent=camera.ui,add_to_scene_entities=add_to_scene_entities, **kwargs)
         
-        self.play_button = Button(text="Play",text_origin=Vec2(-.3,0),model="quad",texture="button",scale=(.3,.1),position=(0,0.1),color=color.white)
-        self.settings_button = Button(text="Settings",scale=0.1,position=(0,-0.1))
+        self.play_button = Button(text="Play",text_origin=Vec2(-.3,0),model="quad",texture="button",scale=(.3,.1),position=(-.5,-0.15),color=color.white)
+        self.settings_button = Button(text="Settings",text_origin=Vec2(-.3,0),model="quad",texture="button",scale=(.3,.1),position=(-.5,-.3),color=color.white)
         
 class SettingsMenu(Entity):
     def __init__(self, add_to_scene_entities=True, **kwargs):

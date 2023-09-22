0e1b4fdd37e24c8203c84142bdd141fdd06f6b5e
Anatole Sot <47571181+ano0002@users.noreply.github.com>
2023-09-23 00:52:16 +0200

    Added a python version check

--- run_game.py
+++ run_game.py
@@ -1,3 +1,13 @@
+import sys
+
+# The major, minor version numbers your require
+MIN_VER = (3, 10)
+
+if sys.version_info[:2] < MIN_VER:
+    sys.exit(
+        "This game requires Python {}.{}.".format(*MIN_VER)
+    )
+
 from bullet import Bullet
 from menu import MainMenu,EndScreen,SettingsMenu
 from random import randint
